30f038b4d3035b2e75469e9150fa06b36bcf1dc7
marc desrosiers <mdesrosiers@norauto.fr>
2017-08-17 16:12:06 +0400

    create models supplier, category and product. Modify home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626165845) do
+ActiveRecord::Schema.define(version: 20170817115625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.float    "price"
+    t.float    "price_per_weight"
+    t.integer  "weight"
+    t.string   "description"
+    t.boolean  "active"
+    t.integer  "category_id"
+    t.integer  "supplier_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["category_id"], name: "index_products_on_category_id", using: :btree
+    t.index ["supplier_id"], name: "index_products_on_supplier_id", using: :btree
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,4 +60,6 @@ ActiveRecord::Schema.define(version: 20170626165845) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "products", "categories"
+  add_foreign_key "products", "suppliers"
 end
