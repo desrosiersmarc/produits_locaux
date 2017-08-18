a2551e13df680b21a4be15277fee9a996faedebe
marc desrosiers <mdesrosiers@norauto.fr>
2017-08-18 15:33:09 +0400

    Create form to create, update supplier

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,3 +1,6 @@
 class Supplier < ApplicationRecord
   has_many :products
+  validates :name, presence: { message: "Il faut mettre un nom quand même... ;o)"},
+    length: {minimum: 4, too_short:"Il faut au minimum 4 caractères",
+            maximum: 45, too_long: "Il faut au maximum 45 caractères"}
 end
