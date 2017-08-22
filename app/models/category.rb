0dc777564c7194c1baf32cb7c34af8a639fbd1ed
marc desrosiers <mdesrosiers@norauto.fr>
2017-08-22 22:56:07 +0400

    Add validation to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_many :products
   has_ancestry
+
+  validates :name, presence: true, length: { maximum: 20, minimum: 2, too_short: "Il faut plus de lettres", too_long:"Trop, c'est trop... ;/"}
 end
