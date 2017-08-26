8ae504319a689faaff416359832b0c3cccec5385
marc desrosiers <mdesrosiers@norauto.fr>
2017-08-26 11:16:47 +0400

    add resize param for cloudinary

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
   def helper_picture_tag(thing)
     if thing.photo?
-      cl_image_tag(thing.photo.path)
+      cl_image_tag(thing.photo.path, width: 250)
     else
-      image_tag("without_picture.jpg")
+      image_tag("without_picture.jpg", width: 250)
     end
   end
 
   def helper_picture_url(thing)
     if thing.photo?
-      cl_image_path(thing.photo.path)
+      cl_image_path(thing.photo.path, width: 250)
     else
-      image_path("without_picture.jpg")
+      image_path("without_picture.jpg", width: 250)
     end
   end
 
