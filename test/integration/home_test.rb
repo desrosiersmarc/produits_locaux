e93d5cd3ec101e13a6622a2f0e4394c5a99d4e88
marc desrosiers <mdesrosiers@norauto.fr>
2017-08-30 14:27:36 +0400

    create first tests

---
+++ test/integration/home_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class HomeTest < ActionDispatch::IntegrationTest
+  # setup
+  setup do
+    @categories = categories(:one)
+    @products = products(:one)
+  end
+
+  test "loads home page and first category page correctly" do
+  # exercise
+    visit "/"
+  # verify
+    assert_equal 200, page.status_code
+    assert page.has_content?("Chez Marie")
+    assert page.has_content?("Viande")
+
+    click_on 'Viande'
+#    assert page.has_content?("Tomates")
+#    save_and_open_screenshot
+  # teardown
+  end
+
+end
