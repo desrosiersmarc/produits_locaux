be8a288ab76fb07a11a83fda93ac4e813e7217d5
marc desrosiers <mdesrosiers@norauto.fr>
2017-09-02 19:17:42 +0400

    update code of Order Item and completed its tests

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -10,4 +10,18 @@ class OrderItemTest < ActiveSupport::TestCase
     assert_equal 1, order_item.order.id
     assert_equal 3, OrderItem.count
   end
+
+  test 'Persisted price' do
+    #Add a order_item
+    order_item = OrderItem.create(product_id: 1,
+                                  order_id: 1,
+                                  quantity: 6)
+    #Test the price in order item
+    assert_equal 5, order_item.unit_price
+    #change the price of the product
+    Product.find(1).update(price: 10)
+    #Test if the price is stil the first price.
+    assert_equal 5, order_item.unit_price
+
+  end
 end
