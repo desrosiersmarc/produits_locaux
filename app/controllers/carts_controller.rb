ce297d340d64681e5197775f8e70f0e50c63f55a
marc desrosiers <mdesrosiers@norauto.fr>
2017-10-02 08:33:26 +0200

    add a condition to display order itme with more than 0

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,8 @@
 class CartsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
   def show
-    @order_items = current_order.order_items.order("created_at")
+    @order_items = current_order.order_items
+                                .where('quantity > 0')
+                                .order("created_at")
   end
 end
