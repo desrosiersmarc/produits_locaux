d3479f1e204e7dc854a946d40a511585bf449a81
marc desrosiers <mdesrosiers@norauto.fr>
2017-10-23 22:17:24 +0200

    work on order update for administrator and update order for cart in controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,10 +3,9 @@ class OrdersController < ApplicationController
 
   def index
     @orders_validated = Order.all.where(order_status_id: 2)
-    @orders_ready = Order.all.where(order_status_id: 3)
     @orders_in_preparation = Order.all.where(order_status_id: 3)
+    @orders_ready = Order.all.where(order_status_id: 4)
     @orders_cancelled = Order.all.where(order_status_id: 6)
-
   end
 
   def show
@@ -15,11 +14,18 @@ class OrdersController < ApplicationController
 
 
   def update
-    @order.order_status_id+=1
-    @order.delivery_date = session[:delivery_day]
-    @order.user = current_user
-    @order.save
-    session[:order_id] = nil
+    if @order.order_status_id == 1
+      @order.order_status_id = 2
+      @order.delivery_date = session[:delivery_day]
+      @order.user = current_user
+      session[:order_id] = nil
+      @order.save
+      # redirect_to order_path
+    else
+      @order.order_status_id = params[:order][:order_status]
+      @order.save
+      redirect_to orders_path
+    end
   end
 
 private
