7c0d712359065fe90f1c127327c4f9543e519ee7
marc desrosiers <mdesrosiers@norauto.fr>
2017-10-25 07:27:47 +0200

    delete user_params because it s not necessary

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,4 @@ private
   def find_user
     @user = User.find(params[:id])
   end
-
-  def user_params
-    params.require(:users).permit(
-      :firstname,
-      :lastname,
-      :mobil_phone)
-  end
 end
