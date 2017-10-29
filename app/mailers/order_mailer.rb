b518cbef7275223f3df92375c4f2fff3b1c3967f
marc desrosiers <mdesrosiers@norauto.fr>
2017-10-29 13:39:45 +0100

    not display quantity = 0 on email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,6 +1,7 @@
 class OrderMailer < ApplicationMailer
   def validated_order(user)
     @user = user
+    @order_items = @user.orders.last.order_items.where('quantity >0')
     mail(to: @user.email, subject: 'Prise en compte de votre commande')
   end
 end
