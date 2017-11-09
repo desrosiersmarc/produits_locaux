34682b15cdb27b23f45a9038dfecadbd83ebf2aa
marc desrosiers <mdesrosiers@norauto.fr>
2017-11-09 23:14:45 +0100

    add link to animated cdn and update add basket animation

--- app/assets/javascripts/add_basket.js
+++ app/assets/javascripts/add_basket.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $('.card-add-basket').click(function(){
     var productName = $(this);
     var idToHide = '#' + productName[0].id;
-    $(idToHide).hide().delay(900).slideDown();
+    $(idToHide).addClass('animated bounce');
+    // $(idToHide).delay(4000).removeClass('hinge');
   });
 });
