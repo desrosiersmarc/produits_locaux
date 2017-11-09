991109ed8471ea5a85b94f8fe360b47b9d2a625e
marc desrosiers <mdesrosiers@norauto.fr>
2017-11-09 14:34:23 +0100

    add an animation to action add basket

---
+++ app/assets/javascripts/add_basket.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.card-add-basket').click(function(){
+    var productName = $(this);
+    var idToHide = '#' + productName[0].id;
+    console.log(idToHide);
+    $(idToHide).hide();
+    $(idToHide).fadeIn(3000);
+  });
+});
