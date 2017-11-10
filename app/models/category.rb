3deeddad9d6e9126783f070145f89b07a5b04a4f
marc desrosiers <mdesrosiers@norauto.fr>
2017-11-10 20:15:31 +0100

    Update category limit

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   has_many :products
   has_ancestry
 
-  validates :name, presence: true, length: { maximum: 20, minimum: 2, too_short: "Il faut plus de lettres", too_long:"Trop, c'est trop... ;/"}
+  validates :name, presence: true, length: { maximum: 25, minimum: 2, too_short: "Il faut plus de lettres", too_long:"Trop, c'est trop... ;/"}
 
   def active_products
     self.products.where(active: true)
