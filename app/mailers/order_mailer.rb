c42db17f7f6e52f14b34a6874240c2613b1d6e47
marc desrosiers <mdesrosiers@norauto.fr>
2017-11-11 00:22:38 +0100

    Improve email template

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,13 +1,15 @@
 class OrderMailer < ApplicationMailer
   def validated_order(user)
     @user = user
+    @order = Order.last
     @order_items = @user.orders.last.order_items.where('quantity >0')
-    mail(to: @user.email, subject: 'Prise en compte de votre commande')
+    mail(to: @user.email, subject: "Prise en compte de votre commande n°#{@order.id}")
   end
 
   def ready_order(user)
     @user = user
+    @order = Order.last
     @order_items = @user.orders.last.order_items.where('quantity >0')
-    mail(to: @user.email, subject: 'Votre commande est prête')
+    mail(to: @user.email, subject: "Votre commande n°#{@order.id} est prête")
   end
 end
--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -3,7 +3,16 @@ class OrderMailerPreview < ActionMailer::Preview
 
   def validated_order()
     @user = User.last
+    @order = Order.last
     @order_items = @user.orders.last.order_items.where('quantity >0')
     OrderMailer.validated_order(@user)
+  end
+
+
+  def ready_order()
+    @user = User.last
+    @order = Order.last
+    @order_items = @user.orders.last.order_items.where('quantity >0')
+    OrderMailer.ready_order(@user)
   end
 end
