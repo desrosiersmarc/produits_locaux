490e4708e239953f3e0b3f45004b9e79e2b1b2fe
marc desrosiers <mdesrosiers@norauto.fr>
2017-11-12 20:49:09 +0100

    adjust delivery date position, animation and optimize picture size upload

--- app/assets/javascripts/move_delivery_date_div.js
+++ app/assets/javascripts/move_delivery_date_div.js
@@ -1,11 +1,15 @@
 var divDay = document.querySelector('.delivery-day');
 $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
-    if (scroll < 71) {
+    if (scroll < 72) {
+      $('.delivery-day').removeClass('animated flash');
       delta = 70 - scroll;
       deltaPx = delta + "px";
-      if (delta > 0) {
+      if (delta > -2) {
         divDay.style.top = deltaPx;
       }
-    }
+    } else {
+      divDay.style.top = "0px";
+      $('.delivery-day').addClass('animated flash');
+    };
 });
